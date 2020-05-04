a0da24764b62d7a5da212772a4d5d35385b4c2aa
Ethan Beyer <ethan@ethanbeyer.com>
2020-05-04 11:46:12 -0400

    updates mysql and php setup

--- stack/mysql.sh
+++ stack/mysql.sh
@@ -16,23 +16,27 @@ MYSQL_SITE_PASS=''
 
 # MySql
 printf "\n${GREEN}Configuring MySQL...${NC}\n\n\n"
-sudo mysql_secure_installation
-sudo phpenmod mbstring
-sudo systemctl restart apache2
 
 sudo service mysql stop
+
 mkdir -p $MYSQL_CONFIG_DIR
+touch $MYSQL_INIT_FILE
 echo "UPDATE mysql.user SET Password = PASSWORD('${PASS}') WHERE User = 'root';" > $MYSQL_INIT_FILE
-echo "CREATE USER '${MYSQL_SITE_USER}'@'localhost' IDENTIFIED BY 'password';" >> $MYSQL_INIT_FILE
+echo "CREATE USER '${MYSQL_SITE_USER}'@'localhost' IDENTIFIED BY '${MYSQL_SITE_PASS}';" >> $MYSQL_INIT_FILE
 echo "GRANT ALL PRIVILEGES ON * . * TO '${MYSQL_SITE_USER}'@'localhost';" >> $
 echo "FLUSH PRIVILEGES;" >> $MYSQL_INIT_FILE
 
 chown mysql:mysql $MYSQL_INIT_FILE
 mysqld_safe --init-file=$MYSQL_INIT_FILE &
-rm $MYSQL_INIT_FILE
 
 # confirm it works
 mysql -u root --password=$PASS
 > quit;
 
+sudo service mysql stop
+
+sudo mysql_secure_installation
+
 sudo service mysql start
+
+rm $MYSQL_INIT_FILE
