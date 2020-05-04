9ddff279c4ee629a8c14a9189e861bea9c1b49de
Ethan Beyer <ethan@ethanbeyer.com>
2020-05-04 11:57:35 -0400

    updates the mysql setup

--- stack/mysql.sh
+++ stack/mysql.sh
@@ -21,16 +21,15 @@ sudo service mysql stop
 
 mkdir -p $MYSQL_CONFIG_DIR
 touch $MYSQL_INIT_FILE
-echo "UPDATE mysql.user SET Password = PASSWORD('${PASS}') WHERE User = 'root';" > $MYSQL_INIT_FILE
+echo "UPDATE USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';" > $MYSQL_INIT_FILE
 echo "CREATE USER '${MYSQL_SITE_USER}'@'localhost' IDENTIFIED BY '${MYSQL_SITE_PASS}';" >> $MYSQL_INIT_FILE
 echo "GRANT ALL PRIVILEGES ON * . * TO '${MYSQL_SITE_USER}'@'localhost';" >> $
 echo "FLUSH PRIVILEGES;" >> $MYSQL_INIT_FILE
 
-chown mysql:mysql $MYSQL_INIT_FILE
 mysqld_safe --init-file=$MYSQL_INIT_FILE &
 
 # confirm it works
-mysql -u root --password=$PASS
+mysql -u root --password=$MYSQL_ROOT_PASS
 > quit;
 
 sudo service mysql stop
