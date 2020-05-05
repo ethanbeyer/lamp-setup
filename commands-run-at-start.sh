df2018d8f51d2deb9fe95bb216886ecfe82d1975
Ethan Beyer <ethan@ethanbeyer.com>
2020-05-04 23:04:00 -0400

    self-docs

---
+++ commands-run-at-start.sh
@@ -0,0 +1,35 @@
+sudo add-apt-repository ppa:ondrej/php
+sudo add-apt-repository ppa:ondrej/apache2
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt update
+sudo apt upgrade
+sudo apt full-upgrade
+sudo apt autoremove
+sudo apt install \
+  apache2 \
+  certbot \
+  curl \
+  mysql-client \
+  mysql-server \
+  openssh-server \
+  php7.4 \
+  php7.4-bcmath \
+  php7.4-bz2 \
+  php7.4-cli \
+  php7.4-common \
+  php7.4-curl \
+  php7.4-fpm \
+  php7.4-gd \
+  php7.4-gettext \
+  php7.4-intl \
+  php7.4-mbstring \
+  php7.4-mysql \
+  php7.4-readline \
+  php7.4-xml \
+  php7.4-zip \
+  python-certbot-apache \
+  screen \
+  software-properties-common \
+  unzip \
+  wget \
+  zip
