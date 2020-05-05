70afc74bd6d28dcd4d6f62526996be6185c6f648
Ethan Beyer <ethan@ethanbeyer.com>
2020-05-05 12:26:47 -0400

    apache setup, continues

--- stack/apache.sh
+++ stack/apache.sh
@@ -19,8 +19,25 @@ cp -r ~/setup/h5bp-apache/* ~/configs/apache2
 
 printf "\n${GREEN}Configuring Apache...${NC}\n\n\n"
 sudo service apache2 stop
-sudo a2enmod setenvif headers deflate filter expires rewrite include proxy_fcgi
-sudo a2disconf *
+
+# we need these mods in order for a lot of things to function
+sudo a2enmod \
+    setenvif \
+    headers \
+    deflate \
+    filter \
+    expires \
+    rewrite \
+    include \
+    proxy_fcgi
+
+# these are the configs that came pre-installed
+sudo a2disconf \
+    charset \
+    other-vhosts-access-log \
+    localized-error-pages \
+    security \
+    serve-cgi-bin
 # sudo /etc/init.d/apache2 restart
 # sudo a2enconf php7.4-fpm
 
